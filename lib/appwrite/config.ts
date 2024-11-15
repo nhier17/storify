aa19bc5cca76156644166d9f04d7c6cb30ec2406
Abraham <abrahamnhier97@gmail.com>
2024-11-15 18:28:33 +0300

    setting up appwrite

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+    databaseId: process.env.APPWRITE_DATABASE_ID!,
+    userCollectionId: process.env.APPWRITE_USER_COLLECTION_ID!,
+    fileCollectionId: process.env.APPWRITE_FILE_COLLECTION_ID!,
+    storageId: process.env.APPWRITE_STORAGE_ID!,
+    secretKey: process.env.APPWRITE_SECRET!,
+}
